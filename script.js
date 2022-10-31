ce2019afe157a304f1c9b3a8f3d5d120e45fd9a5
EwanColquhoun <ewancolquhoun@hotmail.com>
2022-10-31 18:44:30 +0000

    Added basic pricing

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 window.addEventListener('DOMContentLoaded', function() {
+    setUp();
+});
+
+function setUp(){
     let initial = 0;
     let final = 0;
     let click=0
@@ -13,29 +17,14 @@ window.addEventListener('DOMContentLoaded', function() {
             if (initial !=0 && final !=0 ) {
                 console.log(initial, final, 'I & F')
                 console.log(click, 'click count')
-                calculateQuote(initial, final); 
+                let length = calculateLength(initial, final); 
+                calculateQuote(length);
+                quoteButton();
             } else {
                 alert('No dates stored');
             }
     }})
-});
-
-
-    // let finalDate = getDateOne();
-    // console.log(finalDate, 'finaldate');
-    // let finalDate = getDateTwo(initialDate);
-    // // calculateQuote(initialDate, finalDate);
-    // let clicks = 0;
-    
-    // document.addEventListener(keydown, function() {
-    //     if (clicks == 0){
-    //         console.log('oneclick')
-    //     } else{
-    //         console.log('otherclicks', clicks)
-    //     }
-    //     ++clicks;
-    // });
-
+}
 
 
 // gets intial date
@@ -53,59 +42,22 @@ function getDateOne(click) {
         dateOne = new Date(myCalender.value.toDateString())
         console.log(dateOne, 'dateone');
         return dateOne;
-        // let finalDate = getDateTwo(dateOne);
-        // return finalDate;
-    // })
-    // return dateOne;
 }
 
-
+// Gets second date
 function getDateTwo(click) {
     click++
-    let button = document.getElementById('quote-button');
     let dateTwo = 'A';
-    // dateTwo = button.addEventListener('click', function() {
         console.log('clicked second time!');
         dateTwo = new Date(myCalender.value.toDateString())
         console.log(dateTwo, 'datetwo');
         return dateTwo;
-        // let finalDate = getDateTwo(dateOne);
-        // return finalDate;
-    // })
-    // return dateOne;
 }
 
 
-// // gets final date
-// function getDateTwo (dateOne) {
-//     console.log('getDateTwoCalled,', dateOne, '= dateOnefrom2')
-
-//     button.addEventListener('click', function(dateOne) {
-//         alert(`Hello ${dateOne}`)
-//         let endDate = myCalender.value;
-//         let dateTwo = new Date(endDate);
-//         console.log(dateTwo, 'datetwo')
-//         return this.dateTwo;
-//     })
-// }
-
-// function secondDate (dateOne){
-//     if (dateOne) {
-            
-//         button.addEventListener('click', function(dateOne) {
-//             alert(`Hello ${dateOne}`)
-//             let endDate = myCalender.value;
-//             let dateTwo = new Date(endDate);
-//             console.log(dateTwo, 'datetwo')
-//             return this.dateTwo;
-//         }) 
-//     } console.log (dateTwo);
-// }
-
-// should compare two and return difference in days
-function calculateQuote(initialDate, finalDate) {
-    console.log('calculateQuoteCalled')
-    // let results = document.getElementById('results');
+// Calculates the number of days
+function calculateLength(initialDate, finalDate) {
+    console.log('calculateLengthCalled')
     console.log(initialDate, finalDate, 'Ini Fin')
     var Difference_In_Time = finalDate.getTime() - initialDate.getTime();
     // To calculate the no. of days between two dates
@@ -115,41 +67,28 @@ function calculateQuote(initialDate, finalDate) {
     Total days = ${Difference_In_Days}
     `
     console.log(Difference_In_Days, 'difference in days');
+    return Difference_In_Days
 }
 
+
+// Works out cost and applys discount
+function calculateQuote(length){
+    let dayRate = 29;
+
+    let total = dayRate * length
+
+    results.innerHTML = `
+    <p>Total Price = <strong>£ ${total}</strong></P>
+    <button id="quote-button-final">Call for availability!</button>
+    `
+}
+
+function quoteButton(){
+    let quoteButton = document.getElementById('quote-button-final');
+    quoteButton.addEventListener('click', function(){
+        alert('Call Jezza on 1800 KITCHENS!');
+    })
+}
 const myCalender = new CalendarPicker('#myCalendarWrapper', {
     // options here
 });
-
-// geeks for geeks Date comparison
-	// JavaScript program to illustrate
-	// calculation of no. of days between two date
-
-	// To set two dates to two variables
-// 	var date1 = new Date("06/30/2019");
-// var date2 = new Date("07/30/2019");
-
-// // To calculate the time difference of two dates
-// var Difference_In_Time = date2.getTime() - date1.getTime();
-
-// // To calculate the no. of days between two dates
-// var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
-
-// //To display the final no. of days (result)
-// document.write("Total number of days between dates <br>"
-// 			+ date1 + "<br> and <br>"
-// 			+ date2 + " is: <br> "
-// 			+ Difference_In_Days);
-
-
-
-
-
-// const currentDateElement = document.getElementById('current-date');
-// currentDateElement.textContent = myCalender.value;
-
-// const currentDayElement = document.getElementById('current-day');
-// currentDayElement.textContent = myCalender.value.getDay();
-
-// const currentToDateString = document.getElementById('current-datestring');
-// currentToDateString.textContent = myCalender.value.toDateString();
